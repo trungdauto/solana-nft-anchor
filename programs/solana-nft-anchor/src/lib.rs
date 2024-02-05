b656573ac1a1cb0e51137ad74db73a80fc9316e6
trungdauto <158998530+trungdauto@users.noreply.github.com>
2024-02-05 14:45:00 +0700

    update nft program address

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("DLwjLAHGgzJkP63K4MipjwzcheV73otJs8wwXJurGdSS");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
